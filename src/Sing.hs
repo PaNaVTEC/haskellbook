e8a460c525b03336eeaa73feee5911225108b0b4
Christian Panadero <panavtec@gmail.com>
2017-10-01 16:47:14 +0100

    Fix signature

--- src/Sing.hs
+++ src/Sing.hs
@@ -6,6 +6,7 @@ fstString x = x ++ " in the rain"
 sndString :: [Char] -> [Char]
 sndString x = x ++ " over the rainbow"
 
-sing = if (x > y) then fstString x else sndString y
+sing :: [Char]
+sing = if x > y then fstString x else sndString y
       where x = "Singin"
             y = "Somewhere"
