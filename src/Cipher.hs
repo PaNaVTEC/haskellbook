30eea65685cca1b4623bcbdff4d078da8de401d3
Christian Panadero <panavtec@gmail.com>
2017-10-01 20:44:19 +0100

    Add cipher

---
+++ src/Cipher.hs
@@ -0,0 +1,25 @@
+module Cipher where
+
+import           Data.Char
+import           Data.List
+import           Data.Maybe
+
+shift :: Int
+shift = 3
+
+isUppercase :: Char -> Bool
+isUppercase c = (ord c) `elem` [97..122]
+--
+isLowercase :: Char -> Bool
+isLowercase c = (ord c) `elem` [65..90]
+
+addShift ::  Char -> [Char] -> Char
+addShift x range = cycle range !! (fromJust(elemIndex x range) + shift)
+
+convertLetter :: Char -> Char
+convertLetter x | isUppercase x = addShift x ['a'..'z']
+convertLetter x | isLowercase x = addShift x ['A'..'Z']
+convertletter otherwise = otherwise
+
+caesarCipher :: String -> String
+caesarCipher = map (\x -> convertLetter x)
