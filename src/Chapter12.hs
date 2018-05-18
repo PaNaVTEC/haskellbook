def593c66dad5bd42ff87913a0509c868950d4d4
Christian Panadero <panavtec@gmail.com>
2018-05-18 22:08:27 +0100

    countTHebowel

--- src/Chapter12.hs
+++ src/Chapter12.hs
@@ -14,3 +14,14 @@ replaceThe text = unwords $ map replace (words text)
   where
     replace :: String -> String
     replace word = maybe "a" id (notThe word)
+
+countTheBeforeVowel :: String -> Integer
+countTheBeforeVowel text = (go 0) $ words text
+  where
+    go :: Integer -> [String] -> Integer
+    go acc []                = acc
+    go acc ("the" : word : rest) = (toInt . startsWithVowel $ word) + (go acc rest) + acc
+    go acc (_ : rest)            = acc + go acc rest
+    startsWithVowel []     = False
+    startsWithVowel (l: _) = l == 'a' || l == 'e' || l == 'i' || l == 'o' || l == 'u'
+    toInt i = if i == True then 1 else 0
