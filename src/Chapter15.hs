bb7344210915ca511ef2e8814c938cb3fe7e1410
Christian Panadero <panavtec@gmail.com>
2018-05-28 19:21:29 +0100

    Semigrup instances

--- src/Chapter15.hs
+++ src/Chapter15.hs
@@ -1,6 +1,7 @@
 module Chapter15 where
 
 import           Data.Monoid
+import qualified Data.Semigroup  as S
 import           Test.QuickCheck
 
 data Optional a = Nada | Only a deriving (Eq, Show)
@@ -44,10 +45,7 @@ firstGen = do
   return (First' a)
 
 optionalGen :: (Arbitrary a) => Gen (Optional a)
-optionalGen = frequency
-  [
-    (2, nadaGen)
-  , (5, onlyGen)]
+optionalGen = frequency [(2, nadaGen) , (5, onlyGen)]
 
 nadaGen :: (Arbitrary a) => Gen (Optional a)
 nadaGen = return Nada
@@ -67,3 +65,88 @@ monoidRightIdentity a = (a <> mempty <> a) == a
 
 monoidAssoc :: (Monoid m, Eq m) => m -> m -> m -> Bool
 monoidAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
+
+-- Semigroups
+data Trivial = Trivial deriving (Eq, Show)
+
+instance S.Semigroup Trivial where
+  _ <> _ = Trivial
+
+instance Arbitrary Trivial where
+  arbitrary = return Trivial
+
+type TrivAssoc = Trivial -> Trivial -> Trivial -> Bool
+
+-- Identity semigroup
+newtype Identity a = Identity a deriving (Eq, Show)
+instance S.Semigroup (Identity a) where
+  a <> _ = a
+instance Arbitrary a => Arbitrary (Identity a) where
+  arbitrary = do
+    i <- arbitrary
+    return $ Identity i
+type IdentityAssoc a = Identity a -> Identity a -> Identity a -> Bool
+
+-- Tuple semigroup
+data Two a b = Two a b deriving (Eq, Show)
+instance (S.Semigroup a, S.Semigroup b) => S.Semigroup (Two a b) where
+  (Two a b) <> (Two c d) = Two (a S.<> c) (b S.<> d)
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
+  arbitrary = do
+    x <- arbitrary
+    y <- arbitrary
+    return $ Two x y
+instance Arbitrary a => Arbitrary (S.First a) where
+  arbitrary = do
+    x <- arbitrary
+    return (S.First x)
+type TwoAssoc a b = (Two a b) -> (Two a b) -> (Two a b) -> Bool
+
+-- Thriple semigroup
+data Three a b c = Three a b c deriving (Eq, Show)
+instance (S.Semigroup a, S.Semigroup b, S.Semigroup c) => S.Semigroup (Three a b c) where
+  (Three a b c) <> (Three d e f) = case (Two a b) S.<> (Two d e) of
+    (Two _a _b) -> Three _a _b (c S.<> f)
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c) => Arbitrary (Three a b c) where
+  arbitrary = do
+    x <- arbitrary
+    y <- arbitrary
+    z <- arbitrary
+    return $ Three x y z
+type ThreeAssoc a b c = (Three a b c) -> (Three a b c) -> (Three a b c) -> Bool
+
+-- Four semigroup
+data Four a b c d = Four a b c d deriving (Eq, Show)
+instance ( S.Semigroup a
+         , S.Semigroup b
+         , S.Semigroup c
+         , S.Semigroup d) => S.Semigroup (Four a b c d) where
+  (Four a b c d) <> (Four e f g h) =
+    case ((Two a b) S.<> (Two e f), (Two c d) S.<> (Two g h)) of
+    ((Two _a _b), (Two _c _d)) -> (Four _a _b _c _d)
+
+instance (Arbitrary a
+         ,Arbitrary b
+         ,Arbitrary c
+         ,Arbitrary d) => Arbitrary (Four a b c d) where
+  arbitrary = do
+    x <- arbitrary
+    y <- arbitrary
+    z <- arbitrary
+    a <- arbitrary
+    return $ Four x y z a
+
+type FourAssoc a b c d = (Four a b c d) -> (Four a b c d) -> (Four a b c d) -> Bool
+-- Law check
+semigroupAssoc :: (S.Semigroup m, Eq m) => m -> m -> m -> Bool
+semigroupAssoc a b c = (a S.<> (b S.<> c)) == ((a S.<> b) S.<> c)
+
+mainSemigroup :: IO ()
+mainSemigroup = do
+  _ <- quickCheck (semigroupAssoc :: TrivAssoc)
+  _ <- quickCheck (semigroupAssoc :: (IdentityAssoc Int))
+  _ <- quickCheck (semigroupAssoc :: (TwoAssoc (S.First Int) (S.First Int)))
+  _ <- quickCheck (semigroupAssoc :: (ThreeAssoc (S.First Int) (S.First Int) (S.First Int)))
+  _ <- quickCheck (semigroupAssoc :: (FourAssoc (S.First Int) (S.First Int) (S.First Int) (S.First Int)))
+  return ()
