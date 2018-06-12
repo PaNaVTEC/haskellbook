8cc26e5535a976648cb9b43c99dd758c3e6baa90
Christian Panadero <panavtec@gmail.com>
2018-06-12 19:45:08 +0100

    Applicatives first exercises

---
+++ src/Chapter17.hs
@@ -0,0 +1,43 @@
+module Chapter17 where
+
+import           Data.List (elemIndex)
+
+added :: Maybe Integer
+added = (+3) <$> (lookup 3 $ zip [1,2,3] [4,5,6])
+
+y :: Maybe Integer
+y = lookup 3 $ zip [1,2,3] [4,5,6]
+
+z :: Maybe Integer
+z = lookup 2 $ zip [1,2,3] [4,5,6]
+
+tupled :: (Maybe Integer, Maybe Integer)
+tupled = (,) y z
+
+x :: Maybe Int
+x = elemIndex 3 [1,2,3,4,5]
+
+y' :: Maybe Int
+y' = elemIndex 4 [1,2,3,4,5]
+
+max' :: Int -> Int -> Int
+max' = max
+
+maxed :: Maybe Int
+maxed = max' <$> x <*> y'
+--maxed = Just max' <*> x <*> y'
+
+xs :: [Integer]
+xs = [1,2,3]
+
+ys :: [Integer]
+ys = [4,5,6]
+
+x' :: Maybe Integer
+x' = lookup 3 $ zip xs ys
+
+y'' :: Maybe Integer
+y'' = lookup 2 $ zip xs ys
+
+summed :: Maybe Integer
+summed = sum <$> ((,) <$> x' <*> y'')
