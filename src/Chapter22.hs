8685c13f44fef41c152f9dcbae4e833799b54716
Christian Panadero <panavtec@gmail.com>
2018-07-03 18:01:28 +0100

    functor and applicative instances

--- src/Chapter22.hs
+++ src/Chapter22.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE InstanceSigs #-}
+
 import           Control.Applicative
 import           Data.Char
 
@@ -42,3 +44,25 @@ mtupled = do
   capped <- cap
   revved <- rev
   return (capped, revved)
+
+newtype Reader r a = Reader { runReader :: r -> a }
+
+instance Functor (Reader r) where
+  fmap :: (a -> b) -> Reader r a -> Reader r b
+  fmap f (Reader ra) = Reader $ \r -> f (ra r)
+
+instance Applicative (Reader r) where
+  pure :: a -> Reader r a
+  pure a = Reader $ \r -> a
+
+  (<*>) :: Reader r (a -> b) -> Reader r a -> Reader r b
+  (Reader rab) <*> (Reader ra) = Reader $ \r -> rab r (ra r)
+
+ask :: Reader a a
+ask = Reader id
+
+myLiftA2 :: Applicative f => (a -> b -> c) -> f a -> f b -> f c
+myLiftA2 f fa fb = f <$> fa <*> fb
+
+asks :: (r -> a) -> Reader r a
+asks = Reader
