c0ed24f9e28ef56a78c67878f64cec6b805d8cb4
Christian Panadero <panavtec@gmail.com>
2018-07-12 22:27:29 +0100

    ip6 wip

--- src/Chapter24.hs
+++ src/Chapter24.hs
@@ -5,6 +5,7 @@ module Chapter24 where
 import           Control.Applicative
 import           Data.Int
 import           Data.Word
+import           Numeric             (readHex)
 import           Test.Hspec
 import           Text.Trifecta
 
@@ -87,3 +88,43 @@ parseIp = do
   where
     parseSegment :: Parser Word32
     parseSegment = read <$> some digit
+
+data IPAddress6 = IPAddress6 Word64 Word64 deriving (Eq, Ord, Show)
+parseIp6 :: Parser IPAddress6
+parseIp6 = do
+  f <- (\a b c d -> (a + b + c + d) :: Word64) <$> parseSegment <*> parseSegment <*> parseSegment <*> parseSegment
+--  t <- (\a b c d -> a + b + c + d) <$> parseSegment <*> parseSegment <*> parseSegment <*> parseSegment
+  return $ IPAddress6 f 1
+  where
+    parseSegment :: Parser Integer
+    parseSegment = try (constP hexSegment separator)
+      <|> const 0 <$> separator
+    hexSegment :: Parser Integer
+    hexSegment = fst . head . readHex <$> (some alphaNum)
+    separator :: Parser ()
+    separator = try (const () <$> char ':') <|> eof
+
+constP :: (Parsing m, Monad m) => m a -> m b -> m a
+constP ma mb = ma >>= (\a -> const a <$> mb)
+
+ip6ToInteger :: IPAddress6 -> Integer
+ip6ToInteger (IPAddress6 q r) = (toInteger q) * (toInteger (maxBound :: Word)) + (toInteger r)
+
+testIp6 :: IO ()
+testIp6 = hspec $ do
+  describe "Tests ip6 addresses" $ do
+    let ip1 = "FE80::0202:B3FF:FE1E:8329"
+        ip2 = "0:0:0:0:0:ffff:cc78:f"
+        ip3 = "ABCD:EF01:2345:6789:ABCD:EF01:2345:6789"
+
+    it ("can parse " ++ ip1) $ do
+      let (Success x) = parseString parseIp6 mempty ip1
+      ip6ToInteger x `shouldBe` 338288524927261089654163772891438416681
+
+    it ("can parse " ++ ip2) $ do
+      let (Success x) = parseString parseIp6 mempty ip2
+      ip6ToInteger x `shouldBe` 281474112159759
+
+    it ("can parse " ++ ip3) $ do
+      let (Success x) = parseString parseIp6 mempty ip3
+      ip6ToInteger x `shouldBe` 228367255721259907113560617832333443072
