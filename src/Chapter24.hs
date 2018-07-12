df1d3c8e8235478cf088a197e06f242a036462a9
Christian Panadero <panavtec@gmail.com>
2018-07-12 15:37:06 +0100

    canadian phone numbers

--- src/Chapter24.hs
+++ src/Chapter24.hs
@@ -3,6 +3,7 @@
 module Chapter24 where
 
 import           Control.Applicative
+import           Test.Hspec
 import           Text.Trifecta
 
 parseDigit :: Parser Char
@@ -17,3 +18,50 @@ base10Integer' = parseNegative <|> base10Integer
     parseNegative = try (char '-') >> negate <$> base10Integer
 
 -- The rest of the exercises are in AltParsing.hs, LeanParsers.hs, Fractions.hs
+
+type NumberingPlanArea = Int
+type Exchange = Int
+type LineNumber = Int
+
+data PhoneNumber =
+  PhoneNumber NumberingPlanArea Exchange LineNumber deriving (Eq, Show)
+
+parsePhone :: Parser PhoneNumber
+parsePhone = do
+  _ <- skipMany $ try $ char '1' >> char '-'
+
+  _ <- skipMany $ char '('
+  planArea <- digitLength 3
+  _ <- skipMany $ char ')'
+  _ <- try $ whiteSpace
+  _ <- skipMany $ char '-'
+
+  exchange <- digitLength 3
+  _ <- try $ whiteSpace
+  _ <- skipMany $ char '-'
+
+  lineNumber <- digitLength 4
+
+  return $ PhoneNumber planArea exchange lineNumber
+  where
+    digitLength :: Int -> Parser Int
+    digitLength n = read <$> count n digit
+
+test :: IO ()
+test = hspec $ do
+  describe "Parse phone" $ do
+    it "with hyphens" $ do
+      show (parseString parsePhone mempty "123-456-7890")
+        `shouldBe` show (Success (PhoneNumber 123 456 7890))
+
+    it "without hyphens" $ do
+      show (parseString parsePhone mempty "1234567890")
+        `shouldBe` show (Success (PhoneNumber 123 456 7890))
+
+    it "with parenthesys, hypens and spaces" $ do
+      show (parseString parsePhone mempty "(123) 456-7890")
+        `shouldBe` show (Success (PhoneNumber 123 456 7890))
+
+    it "with parenthesys" $ do
+      show (parseString parsePhone mempty "1-123-456-7890")
+        `shouldBe` show (Success (PhoneNumber 123 456 7890))
