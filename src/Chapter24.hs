dc23e9dada4816e6130d50dfcd6ed0d33abf263e
Christian Panadero <panavtec@gmail.com>
2018-07-12 07:58:07 +0100

    placeholder functions

--- src/Chapter24.hs
+++ src/Chapter24.hs
@@ -1,3 +1,11 @@
 {-# LANGUAGE InstanceSigs #-}
 
 module Chapter24 where
+
+import           Text.Trifecta
+
+parseDigit :: Parser Char
+parseDigit = undefined
+
+base10Integer :: Parser Integer
+base10Integer = undefined
