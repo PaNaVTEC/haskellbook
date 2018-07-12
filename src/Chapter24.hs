2bfda8a13ac330f9a3d45ccaa5a80795ed68f1c0
Christian Panadero <panavtec@gmail.com>
2018-07-12 14:51:35 +0100

    negative numbers parser

--- src/Chapter24.hs
+++ src/Chapter24.hs
@@ -2,6 +2,7 @@
 
 module Chapter24 where
 
+import           Control.Applicative
 import           Text.Trifecta
 
 parseDigit :: Parser Char
@@ -9,5 +10,10 @@ parseDigit = choice $ char <$> ['0'..'9']
 
 base10Integer :: Parser Integer
 base10Integer = read <$> many parseDigit
+
+base10Integer' :: Parser Integer
+base10Integer' = parseNegative <|> base10Integer
+  where
+    parseNegative = try (char '-') >> negate <$> base10Integer
 
 -- The rest of the exercises are in AltParsing.hs, LeanParsers.hs, Fractions.hs
