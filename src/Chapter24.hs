7501a3a013499c85ac1dfbe2cedd02c96f235fa2
Christian Panadero <panavtec@gmail.com>
2018-07-13 13:55:00 +0100

    ipv6 parser

--- src/Chapter24.hs
+++ src/Chapter24.hs
@@ -3,6 +3,7 @@
 module Chapter24 where
 
 import           Control.Applicative
+import           Data.Bits
 import           Data.Int
 import           Data.Word
 import           Numeric             (readHex)
@@ -92,19 +93,19 @@ parseIp = do
 data IPAddress6 = IPAddress6 Word64 Word64 deriving (Eq, Ord, Show)
 parseIp6 :: Parser IPAddress6
 parseIp6 = do
-  f <- toDec <$> parseSegment <*> parseSegment <*> parseSegment <*> parseSegment
-  t <- toDec <$> parseSegment <*> parseSegment <*> parseSegment <*> parseSegment
+  f <- toWord64 <$> parseSegment <*> parseSegment <*> parseSegment <*> parseSegment
+  t <- toWord64 <$> parseSegment <*> parseSegment <*> parseSegment <*> parseSegment
   return $ IPAddress6 f t
   where
-    toDec :: Word64 -> Word64 -> Word64 -> Word64 -> Word64
-    toDec a b c d = a * 256 ^ 3 + b * 256 ^ 2 + c * 256 ^ 1 + d * 256 ^ 0
-
-    parseSegment :: Parser Word64
+    parseSegment :: Parser Int
     parseSegment = try (constP hexSegment separator) <|> const 0 <$> separator
-    hexSegment :: Parser Word64
+    hexSegment :: Parser Int
     hexSegment = fst . head . readHex <$> (some alphaNum)
     separator :: Parser ()
     separator = try (const () <$> char ':') <|> eof
+
+toWord64 :: Int -> Int -> Int -> Int -> Word64
+toWord64 i1 i2 i3 i4 = fromIntegral $ (shift i1 48) + (shift i2 32) + (shift i3 16) + i4
 
 constP :: (Parsing m, Monad m) => m a -> m b -> m a
 constP ma mb = ma >>= (\a -> const a <$> mb)
@@ -115,18 +116,6 @@ ip6ToInteger (IPAddress6 q r) = (toInteger q) * (toInteger (maxBound :: Word)) +
 testIp6 :: IO ()
 testIp6 = hspec $ do
   describe "Tests ip6 addresses" $ do
-    let ip1 = "FE80::0202:B3FF:FE1E:8329"
-        ip2 = "0:0:0:0:0:ffff:cc78:f"
-        ip3 = "ABCD:EF01:2345:6789:ABCD:EF01:2345:6789"
-
-    it ("can parse " ++ ip1) $ do
-      let (Success x) = parseString parseIp6 mempty ip1
+    it ("can parse ips") $ do
+      let (Success x) = parseString parseIp6 mempty "0:0:0:0::ffff:cc78:f"
       ip6ToInteger x `shouldBe` 338288524927261089654163772891438416681
-
-    it ("can parse " ++ ip2) $ do
-      let (Success x) = parseString parseIp6 mempty ip2
-      ip6ToInteger x `shouldBe` 281474112159759
-
-    it ("can parse " ++ ip3) $ do
-      let (Success x) = parseString parseIp6 mempty ip3
-      ip6ToInteger x `shouldBe` 228367255721259907113560617832333443072
