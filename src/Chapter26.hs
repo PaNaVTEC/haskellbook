fe21d1a18d780df9d236e8aa1eab7e486dfc6d71
Christian Panadero <panavtec@gmail.com>
2018-10-14 15:49:09 +0100

    EitherT exercises

--- src/Chapter26.hs
+++ src/Chapter26.hs
@@ -3,6 +3,7 @@
 module Chapter26 where
 
 import Control.Applicative
+import Control.Monad
 
 newtype EitherT e m a = EitherT { runEitherT :: m (Either e a) }
 
@@ -16,3 +17,23 @@ instance Applicative m => Applicative (EitherT e m) where
 
   (<*>) :: EitherT e m (a -> b) -> EitherT e m a -> EitherT e m b
   (EitherT ab) <*> (EitherT a) = EitherT $ (liftA2 . liftA2) ($) ab a
+
+instance Monad m => Monad (EitherT e m) where
+  return :: a -> EitherT e m a
+  return = pure
+
+  (>>=) :: EitherT e m a -> (a -> EitherT e m b) -> EitherT e m b
+  eita >>= ab = join $ ab <$> eita
+
+swapEitherT :: Functor m => EitherT e m a -> EitherT a m e
+swapEitherT (EitherT ma) = EitherT $ fmap swapEither ma
+
+swapEither :: Either e a -> Either a e
+swapEither (Left e) = Right e
+swapEither (Right a) = Left a
+
+eitherT :: Monad m => (a -> m c) -> (b -> m c) -> EitherT a m b -> m c
+eitherT amc bmc (EitherT amb) = amb >>= mapLR
+  where
+    mapLR (Left a) = amc a
+    mapLR (Right b) = bmc b
