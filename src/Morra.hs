014d08a785c95cf4900c079fdb2bd3ee5e4ab772
Christian Panadero <panavtec@gmail.com>
2018-11-17 22:38:44 +0000

    basic game usage

--- src/Morra.hs
+++ src/Morra.hs
@@ -5,12 +5,12 @@ module Morra where
 
 import           Control.Monad.IO.Class
 import           Control.Monad.State
-import           Data.Maybe             (fromJust, isJust)
+import           Data.Maybe             (maybe, isJust)
 import           System.Random          (randomRIO)
 import           Text.Read              (readMaybe)
 
 main :: IO ()
-main = const () <$> foo
+main = fst <$> runStateT game (GameState (Score 0) (Score 0))
 
 newtype Score = Score { unScore :: Int } deriving (Num, Eq, Show)
 data GameState = GameState {
@@ -27,15 +27,37 @@ readPlayerInput = maybe Nothing mkValidInput . readMaybe <$> liftIO getLine
 
 game :: (MonadState GameState m, MonadIO m) => m ()
 game = do
-  playerInput <- untilInputIsCorrect
-  computerInput <- liftIO $ mkValidInput <$> randomRIO (0 :: Int, 10)
+  _pInput <- untilM isJust readPlayerInput
+  _cInput <- liftIO $ mkValidInput <$> randomRIO (0, 10)
 
-  liftIO . putStrLn $ "P: " ++ show playerInput
-  liftIO . putStrLn $ "C: " ++ show computerInput
-  return ()
+  liftIO $ do
+    putStrLn "Player is odds, computer is evens."
+    putStrLn $ "P: " ++ show _pInput
+    putStrLn $ "C: " ++ show _cInput
 
-untilInputIsCorrect :: MonadIO m => m (Maybe ValidInput)
-untilInputIsCorrect = untilM isJust readPlayerInput
+  maybe
+    (return ())
+    (\eo -> do
+        liftIO $ case eo of
+          Even -> putStrLn "- C wins"
+          Odd -> putStrLn "- P wins"
+        cState <- get
+        put $ incScore cState eo
+    )
+    (calculateEvenOrOdd _pInput _cInput)
+
+  where
+    incScore gameState Even =
+      gameState { computerScore = computerScore gameState + 1 }
+    incScore gameState Odd =
+      gameState { playerScore = playerScore gameState + 1 }
+
+calculateEvenOrOdd :: Maybe ValidInput -> Maybe ValidInput -> Maybe EvenOrOdd
+calculateEvenOrOdd playerInput computerInput = (\a b -> mkEvenOdd $ unInput a + unInput b) <$> playerInput <*> computerInput
+
+data EvenOrOdd = Even | Odd
+mkEvenOdd :: Int -> EvenOrOdd
+mkEvenOdd i = if even i then Even else Odd
 
 untilM :: Monad m => (a -> Bool) -> m a -> m a
 untilM p ma = (\a -> if p a then return a else untilM p ma) =<< ma
